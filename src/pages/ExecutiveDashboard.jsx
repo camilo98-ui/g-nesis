346e5e9a0fe7ba1624a9fcc0c3372c4635da6076
Cristian Camilo Rivera.Cuellar <cristian.rivera@heladospopsy.com>
2025-12-21 02:45:40 +0000

    File changes

--- src/pages/ExecutiveDashboard.jsx
+++ src/pages/ExecutiveDashboard.jsx
@@ -1257,8 +1257,8 @@ INSTRUCCIONES:
             </motion.div>
           )}
           </AnimatePresence>
-            </>
-            )}
+          </>
+          )}
         </div>
       </div>
     </div>
