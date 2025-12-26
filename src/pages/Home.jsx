078b9dc886ab3ba54d4dd1a073f459a520ff054b
Cristian Camilo Rivera.Cuellar <cristian.rivera@heladospopsy.com>
2025-12-26 15:28:08 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -213,6 +213,8 @@ export default function Home() {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [loginSuccess, setLoginSuccess] = useState(false);
   const [showExperienciaPopsy, setShowExperienciaPopsy] = useState(false);
+  const [customerComment, setCustomerComment] = useState('');
+  const [showCommentInput, setShowCommentInput] = useState(false);
 
   const ROLES = [
   { 
