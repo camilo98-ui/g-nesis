5bbefc99285eb79b95bd071791dcc94f2908b584
Cristian Camilo Rivera.Cuellar <cristian.rivera@heladospopsy.com>
2026-01-04 04:34:31 +0000

    File changes

--- src/components/budget/RetailWeekBudgetCard.jsx
+++ src/components/budget/RetailWeekBudgetCard.jsx
@@ -547,6 +547,32 @@ export default function RetailWeekBudgetCard({ dailySales, activeBudget, storeId
                       </ResponsiveContainer>
                       <ResponsiveContainer width="100%" height="100%">
                         <LineChart data={budgetData.last7DaysSales} className="animated-line">
+                          <defs>
+                            <filter id="glowSecond">
+                              <feGaussianBlur stdDeviation="3" result="coloredBlur"/>
+                              <feMerge>
+                                <feMergeNode in="coloredBlur"/>
+                                <feMergeNode in="SourceGraphic"/>
+                              </feMerge>
+                            </filter>
+                            <linearGradient id="lineGradientSecond" x1="0%" y1="0%" x2="100%" y2="0%">
+                              <stop offset="-20%" stopColor="#fff" stopOpacity="0">
+                                <animate attributeName="offset" values="-0.2;-0.2;-0.2;1.2" dur="5s" repeatCount="indefinite"/>
+                              </stop>
+                              <stop offset="-10%" stopColor="#fff" stopOpacity="0.5">
+                                <animate attributeName="offset" values="-0.1;-0.1;-0.1;1.3" dur="5s" repeatCount="indefinite"/>
+                              </stop>
+                              <stop offset="0%" stopColor="#fff" stopOpacity="1">
+                                <animate attributeName="offset" values="0;0;0;1.4" dur="5s" repeatCount="indefinite"/>
+                              </stop>
+                              <stop offset="10%" stopColor="#fff" stopOpacity="0.5">
+                                <animate attributeName="offset" values="0.1;0.1;0.1;1.5" dur="5s" repeatCount="indefinite"/>
+                              </stop>
+                              <stop offset="20%" stopColor="#fff" stopOpacity="0">
+                                <animate attributeName="offset" values="0.2;0.2;0.2;1.6" dur="5s" repeatCount="indefinite"/>
+                              </stop>
+                            </linearGradient>
+                          </defs>
                           <Line 
                             type="monotone" 
                             dataKey="value" 
@@ -555,6 +581,17 @@ export default function RetailWeekBudgetCard({ dailySales, activeBudget, storeId
                             dot={false}
                             strokeOpacity={0.4}
                             isAnimationActive={false}
+                          />
+                          <Line 
+                            type="monotone" 
+                            dataKey="value" 
+                            stroke="url(#lineGradientSecond)" 
+                            strokeWidth={3} 
+                            dot={false}
+                            filter="url(#glowSecond)"
+                            isAnimationActive={true}
+                            animationDuration={2000}
+                            animationEasing="ease-in-out"
                           />
                         </LineChart>
                       </ResponsiveContainer>
