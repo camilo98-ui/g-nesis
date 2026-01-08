7e3f7c6306939de577ba172b68eee53f312cce0f
Cristian Camilo Rivera.Cuellar <cristian.rivera@heladospopsy.com>
2026-01-08 19:05:01 +0000

    File changes

--- src/pages/ExecutiveDashboard.jsx
+++ src/pages/ExecutiveDashboard.jsx
@@ -912,7 +912,7 @@ Genera:
         </div>
       </Link>
 
-      <div className="max-w-[2400px] mx-auto px-6 py-6 relative z-10">
+      <div className="w-full mx-auto px-2 py-6 relative z-10">
         {/* Header Compacto con Filtro de Semanas */}
         <div className="mb-6">
           <div className="flex items-center justify-between gap-4 flex-wrap">
