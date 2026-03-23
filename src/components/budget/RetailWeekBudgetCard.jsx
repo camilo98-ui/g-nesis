8570819f51cc11be40b06235b14fa1e4f5bdde2d
Cristian Camilo Rivera.Cuellar <cristian.rivera@heladospopsy.com>
2026-03-23 18:54:27 +0000

    File changes

--- src/components/budget/RetailWeekBudgetCard.jsx
+++ src/components/budget/RetailWeekBudgetCard.jsx
@@ -631,6 +631,52 @@ export default function RetailWeekBudgetCard({ dailySales, activeBudget, dailyBu
                       </span>
                     </div>
                   )}
+                  
+                  {/* Sparkline debajo del número */}
+                  {budgetData.last7DaysSales?.length > 0 && (
+                    <div className="mt-3 h-10">
+                      <ResponsiveContainer width="100%" height="100%">
+                        <LineChart data={budgetData.last7DaysSales}>
+                          <defs>
+                            <filter id="glow1">
+                              <feGaussianBlur stdDeviation="2" result="coloredBlur"/>
+                              <feMerge>
+                                <feMergeNode in="coloredBlur"/>
+                                <feMergeNode in="SourceGraphic"/>
+                              </feMerge>
+                            </filter>
+                            <linearGradient id="lineGrad1" x1="0%" y1="0%" x2="100%" y2="0%">
+                              <stop offset="-20%" stopColor="#fff" stopOpacity="0">
+                                <animate attributeName="offset" values="-0.2;1.2;1.2" dur="3s" repeatCount="indefinite"/>
+                              </stop>
+                              <stop offset="0%" stopColor="#fff" stopOpacity="1">
+                                <animate attributeName="offset" values="0;1.4;1.4" dur="3s" repeatCount="indefinite"/>
+                              </stop>
+                              <stop offset="20%" stopColor="#fff" stopOpacity="0">
+                                <animate attributeName="offset" values="0.2;1.6;1.6" dur="3s" repeatCount="indefinite"/>
+                              </stop>
+                            </linearGradient>
+                          </defs>
+                          <Line 
+                            type="monotone" 
+                            dataKey="value" 
+                            stroke="#fff" 
+                            strokeWidth={1.5} 
+                            dot={false}
+                            strokeOpacity={0.3}
+                          />
+                          <Line 
+                            type="monotone" 
+                            dataKey="value" 
+                            stroke="url(#lineGrad1)" 
+                            strokeWidth={2.5} 
+                            dot={false}
+                            filter="url(#glow1)"
+                          />
+                        </LineChart>
+                      </ResponsiveContainer>
+                    </div>
+                  )}
                 </div>
 
                 {/* Panel derecho: Brecha total y % de impacto */}
