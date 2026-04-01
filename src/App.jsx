8a87ef007b97ec31ee8ac911954487e266474be4
Cristian Camilo Rivera.Cuellar <cristian.rivera@heladospopsy.com>
2026-04-01 23:01:56 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
-const MainPage = mainPageKey ? Pages[mainPageKey] : <></>;
+const MainPage = mainPageKey ? Pages[mainPageKey] : null;
 
 const LayoutWrapper = ({ children, currentPageName }) => Layout ?
   <Layout currentPageName={currentPageName}>{children}</Layout>
@@ -46,11 +46,11 @@ const AuthenticatedApp = () => {
   // Render the main app
   return (
     <Routes>
-      <Route path="/" element={
+      {MainPage && <Route path="/" element={
         <LayoutWrapper currentPageName={mainPageKey}>
           <MainPage />
         </LayoutWrapper>
-      } />
+      } />}
       {Object.entries(Pages).map(([path, Page]) => (
         <Route
           key={path}
