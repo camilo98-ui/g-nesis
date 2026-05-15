5e458fda244f1b08bbcc985d8ad8c99d7da6dc88
Cristian Camilo Rivera.Cuellar <cristian.rivera@heladospopsy.com>
2026-05-15 17:39:02 +0000

    File changes

--- src/components/MascotCanvas.jsx
+++ src/components/MascotCanvas.jsx
@@ -15,22 +15,17 @@ export default function MascotCanvas({ width = 64, height = 64 }) {
     canvas.width = width;
     canvas.height = height;
 
-    // Load and draw mascot image with white background removed
+    // Draw white circular background (smaller, behind mascot)
+    ctx.fillStyle = '#ffffff';
+    ctx.beginPath();
+    ctx.arc(width / 2, height / 2, width * 0.35, 0, Math.PI * 2);
+    ctx.fill();
+
+    // Load and draw mascot image
     const img = new Image();
     img.crossOrigin = 'anonymous';
     img.onload = () => {
       ctx.drawImage(img, 0, 0, width, height);
-      // Remove white background
-      const imageData = ctx.getImageData(0, 0, width, height);
-      const data = imageData.data;
-      for (let i = 0; i < data.length; i += 4) {
-        const r = data[i], g = data[i + 1], b = data[i + 2];
-        // If pixel is very light/white, make it transparent
-        if (r > 220 && g > 220 && b > 220) {
-          data[i + 3] = 0;
-        }
-      }
-      ctx.putImageData(imageData, 0, 0);
     };
     img.src = MASCOT_IMG;
   }, [width, height]);
